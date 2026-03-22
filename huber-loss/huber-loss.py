f6fae9ae3804ebbcc0215eb4059f65a203386a17
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-03-22 15:53:11 +0530

    Add solution for huber-loss

---
+++ huber-loss/huber-loss.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def huber_loss(y_true, y_pred, delta=1.0):
+    """
+    Compute Huber Loss for regression.
+    """
+    # Write code here
+    y_pred = np.array(y_pred)
+    y_true = np.array(y_true)
+
+    error =  np.abs(y_pred-y_true)
+
+    loss = np.where(
+        error <= delta,
+        0.5 * error**2,
+        delta * (error - 0.5 * delta)
+    )
+
+    return np.mean(loss)
+
+    
